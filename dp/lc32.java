3b8079dbecd4120bfdeb6f63876b41e1512ce3e0
j-tooyoung <arnoldtooyoung@hotmail.com>
2020-07-04 10:52:13 +0800

    submit some solution

--- dp/lc32.java
+++ dp/lc32.java
@@ -1,7 +1,12 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
  * ClassName: lc32
  * Package: PACKAGE_NAME
  * Description:
+ * 32. 最长有效括号
+ * https://leetcode-cn.com/problems/longest-valid-parentheses/
  *
  * @date: 2020/4/8 22:55
  * @author: tooyoung
@@ -9,7 +14,54 @@
 
 public class lc32 {
 
-    public static void main(String[] args) {
+    public static int longestValidParentheses(String s) {
+        // 遇到左括号入账，右括号出栈，将坐标入栈
+        LinkedList<Integer> st = new LinkedList<>();
+        int res = 0;
+        st.addLast(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                st.addLast(i);
+            } else {
+                st.pollLast();
+                if (st.isEmpty()) {
+                    st.addLast(i);
+                } else {
+//                    System.out.println(i - st.getLast());
+                    res = Math.max(res, i - st.getLast());
+                }
+            }
+        }
+        return res;
+    }
 
+
+    public static int longestValidParentheses1(String s) {
+        int len = s.length();
+        int[] dp = new int[len];
+        int res = 0;
+        char[] ch = s.toCharArray();
+        if (ch[0] == '(' && ch[1] == ')') {
+            dp[0] = 1;
+            dp[0] = 1;
+        }
+        for (int i = 2; i < len; i++) {
+            if (ch[i - 2] == '(' && ch[i - 1] == ')') {
+                dp[i] = dp[i - 2] + 2;
+            } else {
+                dp[i] = 0;
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "(())";
+        String s1 = ")()())";
+        String s2 = "(()(()";
+        System.out.println(longestValidParentheses(s));
+        System.out.println(longestValidParentheses(s1));
+        System.out.println(longestValidParentheses(s2));
     }
 }
--- stack/lc20.java
+++ stack/lc20.java
@@ -1,15 +1,48 @@
+import java.util.*;
+
 /**
  * ClassName: lc20
  * Package: PACKAGE_NAME
  * Description:
+ * 20. 有效的括号
+ * https://leetcode-cn.com/problems/valid-parentheses/
  *
  * @date: 2020/4/8 23:10
  * @author: tooyoung
  */
 
 public class lc20 {
-    public static void main(String[] args) {
 
+    // 字符串替代，"()"替代为”“
+    public static boolean isValid(String s) {
+        Map<Character,Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+        LinkedList<Character> st = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                st.addLast(ch);
+            } else {
+                //
+                if (st.isEmpty() || ch != map.get(st.getLast())) {
+                    return false;
+                } else {
+                    st.removeLast();
+                }
+            }
+        }
+        return st.isEmpty() ? true : false;
+    }
+
+    public static void main(String[] args) {
+        String s = "()";
+        String s1 =  "()[]{}";
+        String s2 = "(]";
+        System.out.println(isValid(s));
+        System.out.println(isValid(s1));
+        System.out.println(isValid(s2));
     }
 
 }
