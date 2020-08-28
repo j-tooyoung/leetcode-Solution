f376799a2cd9d1d643fca12a38673944cdeec529
xpfh <xupeng4215@fiberhome.com>
2020-08-28 18:02:49 +0800

    submit

---
+++ Weeklycontest/week120/lc977.java
@@ -0,0 +1,13 @@
+package week120;
+
+/**
+ * 977. 有序数组的平方
+ * https://leetcode-cn.com/contest/weekly-contest-120/problems/squares-of-a-sorted-array/
+ */
+public class lc977 {
+
+    public int[] sortedSquares(int[] A) {
+        return null;
+    }
+
+}
---
+++ Weeklycontest/week120/lc978.java
@@ -0,0 +1,9 @@
+package week120;
+
+/**
+ * https://leetcode-cn.com/contest/weekly-contest-120/problems/longest-turbulent-subarray/
+ *
+ */
+public class lc978 {
+
+}
