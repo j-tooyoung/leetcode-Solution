1335b5e6de381c3eeae370fe104faca2ce055a04
xpfh <xupeng4215@fiberhome.com>
2020-08-31 09:35:28 +0800

    submit lc841

--- graph/lc841.java
+++ graph/lc841.java
@@ -1,4 +1,6 @@
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * DFS, connected components
@@ -8,8 +10,25 @@ import java.util.List;
  */
 public class lc841 {
 
+    // bfs
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        return false;
+        int n = rooms.size();
+        int num = 0;
+        boolean[] vis = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        vis[0] = true;
+        while (!q.isEmpty()) {
+            int tmp = q.poll();
+            num++;
+            for (int val : rooms.get(tmp)) {
+                if (!vis[val]) {
+                    vis[val] = true;
+                    q.add(val);
+                }
+            }
+        }
+        return num == n;
     }
 
 }
