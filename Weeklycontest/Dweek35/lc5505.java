cb2227956b1eef226ae7af8ba875553452e9d00a
j-tooyoung <arnoldtooyoung@hotmail.com>
2020-09-21 22:40:21 +0800

    submit

--- Weeklycontest/Dweek35/lc5505.java
+++ Weeklycontest/Dweek35/lc5505.java
@@ -12,21 +12,27 @@ import java.util.Comparator;
  */
 public class lc5505 {
 
+//    统计每个位置被覆盖的次数，按照次数的高低依次分配数字，次数和数字相乘。
+//    统计被覆盖次数可以采用差分，然后求前缀和的方式。
     public int maxSumRangeQuery(int[] nums, int[][] requests) {
-        Arrays.sort(nums);
-        long res = 0;
-        long mod = 1000_000_007;
-        Arrays.sort(requests, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] t1, int[] t2) {
-                return t1[1] - t1[0] - (t2[1] - t2[0]);
+        int res = 0;
+        int mod = 1000_000_007;
+        int len = nums.length;
+        int[] cnt = new int[len];
+        for (int[] val : requests) {
+            cnt[val[0]]++;
+            if (val[1] + 1 < len) {
+                cnt[val[1] + 1]--;
             }
-        });
-        int idx = nums.length - 1;
-        for (int i = 0; i < requests.length; i++) {
-            int len = requests[i][1] - requests[i][0];
-
         }
-        return (int) (res % mod);
+        for (int i = 1; i < len; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+        Arrays.sort(nums);
+        Arrays.sort(cnt);
+        for (int i = 0; i < len; i++) {
+            res = (int) ((res + (long) (nums[i]) * cnt[i]) % mod);
+        }
+        return res;
     }
 }
