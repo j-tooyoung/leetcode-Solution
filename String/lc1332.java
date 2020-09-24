be73a7c7bd5d7f7a1c135299e15cae96db7138ac
j-tooyoung <arnoldtooyoung@hotmail.com>
2020-09-24 09:16:58 +0800

    submit

--- String/lc1332.java
+++ String/lc1332.java
@@ -5,9 +5,17 @@
 public class lc1332 {
 
     public int removePalindromeSub(String s) {
+        if (s == null ||  s.length() == 0) return 0;
         int len = s.length();
-
-        return 0;
+        int i = 0, j = len - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return 2;
+            }
+            i++;
+            j--;
+        }
+        return 1;
     }
 
     public static void main(String[] args) {
