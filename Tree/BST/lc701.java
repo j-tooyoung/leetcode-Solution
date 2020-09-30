e80cff9bd6aa8c6a170051dff87701e5c4d2c1de
j-tooyoung <arnoldtooyoung@hotmail.com>
2020-09-30 08:56:28 +0800

    submit

--- Tree/BST/lc701.java
+++ Tree/BST/lc701.java
@@ -5,8 +5,12 @@ package BST;
  * https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/
  */
 public class lc701 {
+
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        return null;
+        if (root == null) return new TreeNode(val);
+        if (root.val < val) root.right = insertIntoBST(root.right, val);
+        else root.left = insertIntoBST(root.left, val);
+        return root;
     }
 
 }
