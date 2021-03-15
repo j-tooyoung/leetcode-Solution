7146224d70744dd679fb47a30633ec55048acc61
j-tooyoung <arnoldtooyoung@hotmail.com>
2021-03-15 21:32:04 +0800

    submit lc5690

--- Weeklycontest/week230/lc5690.java
+++ Weeklycontest/week230/lc5690.java
@@ -10,47 +10,89 @@ import java.util.Arrays;
  */
 public class lc5690 {
 
+    int ans = -0x3f3f3f3f;
+
     public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
         int cnt = 0;
         int minVal = Arrays.stream(baseCosts).min().getAsInt();
         if (minVal >= target) {
             return minVal;
         }
-        boolean flag = true;
-        while (flag) {
-            for (int i = 0; i < baseCosts.length; i++) {
-                if (dfs1(target - cnt - baseCosts[i], toppingCosts, 0)) {
-                    flag = false;
-                    return target - cnt;
-                }
-                if (dfs1(target + cnt - baseCosts[i], toppingCosts, 0)) {
-                    flag = false;
-                    return target + cnt;
-                }
-            }
-            ++cnt;
+        int n = baseCosts.length;
+        for (int i = 0; i < n; i++) {
+            dfs1(baseCosts[i], toppingCosts, 0, target);
         }
-        return target;
+        return ans;
     }
 
-    private boolean dfs1(int sum, int[] toppingCosts, int idx) {
-        if(sum <0)return false;
+    private void dfs1(int sum, int[] toppingCosts, int idx, int target) {
+        if (idx > toppingCosts.length) {
+            return;
+        }
         if (idx == toppingCosts.length) {
-            return sum == 0;
+            if (Math.abs(sum - target) < Math.abs(ans - target) || ((ans > sum) && (Math.abs(sum - target) == Math.abs(ans - target)))) {
+                ans = sum;
+            }
+            return;
         }
-        if (dfs1(sum - toppingCosts[idx], toppingCosts, idx + 1)) {
-            return true;
-        }
-        if (dfs1(sum - 2 * toppingCosts[idx], toppingCosts, idx + 1)) {
-            return true;
-        }
-        if (dfs1(sum, toppingCosts, idx + 1)) {
-            return true;
-        }
+        dfs1(sum + toppingCosts[idx], toppingCosts, idx + 1, target);
+        dfs1(sum + 2 * toppingCosts[idx], toppingCosts, idx + 1, target);
+        dfs1(sum, toppingCosts, idx + 1, target);
 
-        return false;
     }
 
-
+//    public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+//        int cnt = 0;
+//        int minVal = Arrays.stream(baseCosts).min().getAsInt();
+//        int maxVal = Arrays.stream(baseCosts).max().getAsInt();
+//        int sum = Arrays.stream(toppingCosts).sum();
+//        if (sum * 2 + maxVal <= target) {
+//            return sum * 2 + maxVal;
+//        }
+//        if (minVal >= target) {
+//            return minVal;
+//        }
+//
+//        boolean flag1 = true;
+//        boolean flag2 = true;
+//        int ans = 0x3f3f3f;
+    // brute force todo
+//        while ((flag1 || flag2) && cnt < 1500) {
+//            for (int i = 0; i < baseCosts.length; i++) {
+//                if (flag1 && dfs1(target - cnt - baseCosts[i], toppingCosts, 0)) {
+//                    flag1 = false;
+//                    if (cnt <= Math.abs(target - ans)) {
+//                        ans = target - cnt;
+//                    }
+//                }
+//                if (flag2 && dfs1(target + cnt - baseCosts[i], toppingCosts, 0)) {
+//                    flag2 = false;
+//                    if (cnt < Math.abs(target - ans)) {
+//                        ans = target + cnt;
+//                    }
+//                }
+//            }
+//            ++cnt;
+//        }
+//        return ans;
+//    }
+//
+//    private boolean dfs1(int sum, int[] toppingCosts, int idx) {
+//        if (sum < 0) return false;
+//        if (idx == toppingCosts.length) {
+//            return sum == 0;
+//        }
+//        if (dfs1(sum - toppingCosts[idx], toppingCosts, idx + 1)) {
+//            return true;
+//        }
+//        if (dfs1(sum - 2 * toppingCosts[idx], toppingCosts, idx + 1)) {
+//            return true;
+//        }
+//        if (dfs1(sum, toppingCosts, idx + 1)) {
+//            return true;
+//        }
+//
+//        return false;
+//    }
 
 }
